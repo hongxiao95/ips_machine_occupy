4997999184a9dd77b790450a0cebfb7fb3f6bb2f
hongxiao95 <hongxiao95@hotmail.com>
2023-12-14 01:37:37 +0900

    machine allocate

--- applymachine.py
+++ applymachine.py
@@ -2,18 +2,172 @@
 from art import text2art
 from colorama import Fore, Back, Style
 import colorama as cl
+import json
+import os
+from datetime import datetime, timedelta
+
+# 初始化 colorama
+cl.init()
+
+def load_gpu_list():
+    """从文件中加载 GPU 列表"""
+    if os.path.exists("gpulist.txt"):
+        with open("gpulist.txt", "r") as file:
+            return [line.strip() for line in file.readlines()]
+    else:
+        return []
+
+def save_gpu_list(gpus):
+    """将 GPU 列表保存到文件"""
+    with open("gpulist.txt", "w") as file:
+        for gpu in gpus:
+            file.write(gpu + "\n")
+
+def load_status():
+    """从文件中加载 GPU 状态。如果文件为空或格式不正确，则返回空列表。"""
+    try:
+        if os.path.exists("status.conf"):
+            with open("status.conf", "r") as file:
+                data = file.read()
+                # 检查文件是否为空
+                if not data:
+                    return []
+                return json.loads(data)
+        else:
+            return []
+    except json.JSONDecodeError:
+        # 如果 JSON 数据格式不正确
+        return []
+
+def save_status(status):
+    """将 GPU 状态保存到文件"""
+    with open("status.conf", "w") as file:
+        json.dump(status, file, indent=4)
+
+def display_status(gpus, status):
+    """展示 GPU 状态"""
+    for gpu in gpus:
+        # 查找 GPU 的当前状态
+        gpu_status = next((item for item in status if item["name"] == gpu and item["end_time"] > datetime.now().isoformat()), None)
+
+        # 如果找到了状态且 GPU 被占用
+        if gpu_status:
+            occupied = Back.RED + text2art(f"{gpu} Occupied") + Style.RESET_ALL
+            print(f"{occupied} by {gpu_status['user']} until {gpu_status['end_time']}\n")
+        else:
+            # 否则认为 GPU 空闲
+            available = Back.GREEN + text2art(f"{gpu} Available") + Style.RESET_ALL
+            print(f"{available}\n")
+
+def add_gpu(gpus):
+    """添加 GPU"""
+    new_gpu = input("Enter the name of the new GPU: ")
+    gpus.append(new_gpu)
+
+def delete_gpu(gpus):
+    """删除 GPU"""
+    gpu_to_delete = input("Enter the name of the GPU to delete: ")
+    gpus.remove(gpu_to_delete)
+
+def occupy_gpu(gpus, status):
+    """抢占 GPU"""
+    print("Available GPUs:")
+    available_gpus = []
+    for gpu in gpus:
+        if not any(item for item in status if item["name"] == gpu and item["end_time"] > datetime.now().isoformat()):
+            available_gpus.append(gpu)
+            print(f"[{len(available_gpus)}] {gpu}")
+
+    if not available_gpus:
+        print("No GPUs available at the moment.")
+        return
+
+    try:
+        choice = int(input("Choose a GPU to occupy (enter number): ")) - 1
+        if choice < 0 or choice >= len(available_gpus):
+            print("Invalid choice.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+
+    gpu_name = available_gpus[choice]
+    user = input("Enter your name: ")
+    duration = int(input("Enter the duration of use in hours (max 240): "))
+    if duration > 240:
+        print("Duration exceeds the maximum limit of 240 hours.")
+        return
+    password = input("Enter a password to release occupation: ")
+    end_time = (datetime.now() + timedelta(hours=duration)).isoformat()
+
+    # 更新状态
+    status.append({"name": gpu_name, "start_time": datetime.now().isoformat(), "end_time": end_time, "user": user, "password": password})
+
+def release_gpu(status):
+    """释放 GPU 占用"""
+    # 展示所有当前被占用的 GPUs
+    occupied_gpus = [gpu for gpu in status if gpu["end_time"] > datetime.now().isoformat()]
+    if not occupied_gpus:
+        print("No GPUs are currently occupied.")
+        return
+
+    print("Occupied GPUs:")
+    for i, gpu in enumerate(occupied_gpus, start=1):
+        print(f"[{i}] {gpu['name']} (occupied by {gpu['user']})")
+
+    try:
+        choice = int(input("Choose a GPU to release (enter number): ")) - 1
+        if choice < 0 or choice >= len(occupied_gpus):
+            print("Invalid choice.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+
+    gpu_to_release = occupied_gpus[choice]
+    password = input("Enter the password to release occupation: ")
+
+    # 检查密码是否正确
+    if gpu_to_release["password"] == password:
+        status.remove(gpu_to_release)
+        print(f"GPU {gpu_to_release['name']} has been released.")
+    else:
+        print("Incorrect password.")
+
 
 def main():
-    # 尝试从gpulist.txt读取GPU列表
-    # 如无GPU列表，请求输入GPU列表，每输入一个就回车确认，如果输入#说明输入完成
-    # 如有GPU列表，尝试从status.conf读取各GPU状态
-    # 展示当前各GPU状态，用art库结合colorama，用红色和绿色显示GPU占用情况，最好有预期占用解除的进度条
-    # 可选择：添加GPU、删除GPU、抢占GPU、续租GPU、解除抢占GPU
-    # 如用户选择抢占GPU，只可以选择抢占当前空闲的CPU，需要输入自己的名字、预计使用时长（小时），最长不超过10天、解除抢占的密码
-    # GPU列表存储在和本python文件同级的文件夹的gpulist.txt文件夹中，每行一个gpu名称
-    # GPU占用情况以json文本的形式存储在本python文件同级的status.conf中，应当是一个json数组，里面每个元素描述一个GPU，是一个map,包含GPU名、起始占用时间、请求的使用时长、占用人、占用人密码等
-    
-    pass
+    gpus = load_gpu_list()
+    if not gpus:
+        print("No GPU list found. Please enter GPU names (type '#' to finish):")
+        while True:
+            gpu_name = input()
+            if gpu_name == '#':
+                break
+            gpus.append(gpu_name)
+        save_gpu_list(gpus)
+
+    status = load_status()
+    display_status(gpus, status)
+
+    # 用户交互部分
+    while True:
+        print("\nOptions: [1] Add GPU [2] Delete GPU [3] Occupy GPU [4] Release GPU [5] Exit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            add_gpu(gpus)
+            save_gpu_list(gpus)
+        elif choice == '2':
+            delete_gpu(gpus)
+            save_gpu_list(gpus)
+        elif choice == '3':
+            occupy_gpu(gpus, status)
+            save_status(status)
+        elif choice == '4':
+            release_gpu(status)
+            save_status(status)
+        elif choice == '5':
+            break
+        display_status(gpus, status)
 
 if __name__ == "__main__":
     main()
